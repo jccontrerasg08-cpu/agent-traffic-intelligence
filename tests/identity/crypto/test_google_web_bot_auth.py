b006f6563b8ea2d27529d3389904483af2d59dac
Juca8 <jccontrerasg08@gmail.com>
2026-08-14 04:46:35 -0700

    style: format Google Web Bot Auth test imports

--- tests/identity/crypto/test_google_web_bot_auth.py
+++ tests/identity/crypto/test_google_web_bot_auth.py
@@ -15,7 +15,6 @@ from agent_traffic_intelligence.identity.models import (
 from agent_traffic_intelligence.identity.sources.trust import SourceTrustPolicy
 from agent_traffic_intelligence.models import ActorType, IdentityClaim
 
-
 IDENTITY_URI = "https://agent.bot.goog"
 DIRECTORY_URI = "https://agent.bot.goog/.well-known/http-message-signatures-directory"
 NOW = datetime(2026, 8, 14, 11, 0, tzinfo=UTC)
