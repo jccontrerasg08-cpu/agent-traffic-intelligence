28b930ff1e5b033ac06e03245e3a8c6232af6aac
Juca8 <jccontrerasg08@gmail.com>
2026-08-14 05:17:13 -0700

    feat(crypto): export cryptographic identity adapters

---
+++ src/agent_traffic_intelligence/identity/crypto/__init__.py
@@ -0,0 +1,24 @@
+"""Optional cryptographic identity verification adapters."""
+
+from agent_traffic_intelligence.identity.crypto.agent_card import AgentCard, AgentCardFormatError, parse_agent_card
+from agent_traffic_intelligence.identity.crypto.directory import DirectoryFormatError, DirectoryKey, KeyDirectory, jwk_thumbprint, parse_key_directory
+from agent_traffic_intelligence.identity.crypto.replay import ReplayCache
+from agent_traffic_intelligence.identity.crypto.rfc9421 import PublicKeyResolver, Rfc9421Result, Rfc9421Verifier
+from agent_traffic_intelligence.identity.crypto.web_bot_auth import WebBotAuthPolicy, WebBotAuthVerifier
+
+__all__ = [
+    "AgentCard",
+    "AgentCardFormatError",
+    "DirectoryFormatError",
+    "DirectoryKey",
+    "KeyDirectory",
+    "PublicKeyResolver",
+    "ReplayCache",
+    "Rfc9421Result",
+    "Rfc9421Verifier",
+    "WebBotAuthPolicy",
+    "WebBotAuthVerifier",
+    "jwk_thumbprint",
+    "parse_agent_card",
+    "parse_key_directory",
+]
